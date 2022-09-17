b79e716dcc0a11ac3d06eaa597c18d93aa1964d3
Enzo Matías Nieto <enzonieto92@hotmail.com>
2022-09-17 15:38:42 -0300

    menu principal 1.0

---
+++ include/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include "rlutil.h"
+#include <cstdio>
+#include <ctime>
+struct origen{
+int x = 0;
+int y = 0;
+};
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "persona.h"
+#include "Cuadro.h"
+#include "Menus.h"
+#include "funciones.h"
+using namespace std;
+
+
+int main()
+{
+rlutil::setColor(12);
+menu_principal();
+
+    return 0;
+}
