eed797f15b0451004d6e18a50cd982538a588409
anthony <anthonydacen@gmail.com>
2022-11-04 16:18:20 -0300

    Cambios Turnos

--- include/Turno.h
+++ include/Turno.h
@@ -9,25 +9,28 @@ class Turno
         Fecha fechaServicio;
         int idCuenta;
         int tipoServicio;
+        int iDpersonal;
         bool estado;
     public:
         // CONSTRUCOR
         Turno();
-        Turno(int, int, int, bool);
+        Turno(int, int, int, int, bool);
         // METODOS
-        bool Cargar(int, Fecha, int, int);
+        bool Cargar(int, Fecha, int, int, int);
         void Mostrar();
         // SETS
         void setIdTurno(int);
         void setFechaServicio(Fecha);
         void setIdCuenta(int);
         void setTipoServicio(int);
+        void setIdPersonal(int);
         void setEstado(bool);
         // GETS
         int getIdTurno();
         Fecha getFechaServicio();
         int getIdCuenta();
         int getTipoServicio();
+        int getIdPersonal();
         bool getEstado();
         // DESTRUCTOR
         ~Turno();
--- src/Turno.cpp
+++ src/Turno.cpp
@@ -8,13 +8,15 @@ Turno::Turno(){
     idTurno = 0;
     idCuenta = 0;
     tipoServicio = 0;
+    iDpersonal = 0;
     estado = false;
 }
 
-Turno::Turno(int id = 0, int d = 0, int ts = 0, bool e = false){
-    idTurno = id;
+Turno::Turno(int idC = 0, int d = 0, int ts = 0, int idP = 0, bool e = false){
+    idTurno = idC;
     idCuenta = d;
     tipoServicio = ts;
+    iDpersonal = idP;
     estado = e;
 }
 
@@ -24,11 +26,12 @@ Turno::~Turno(){
 }
 
 // METODOS
-bool Turno::Cargar(int idT, Fecha _fecha, int idC, int ts){
+bool Turno::Cargar(int idT, Fecha _fecha, int idC, int ts, int idP){
     setIdTurno(idT);
     setFechaServicio(_fecha);
     setIdCuenta(idC);
     setTipoServicio(ts);
+    setIdPersonal(idP);
     estado = 1;
     setEstado(estado);
     return true;
@@ -42,9 +45,11 @@ void Turno::Mostrar(){
     gotoxy(42, 22);
     fechaServicio.MostrarHora();
     gotoxy(42, 23);
-    cout << "ID CUENTA: " << idCuenta << endl;
+    cout << "ID CLIENTE: " << idCuenta << endl;
     gotoxy(42, 24);
     cout << "TIPO DE SERVICIO: " << tipoServicio << endl;
+    gotoxy(42, 25);
+    cout << "ID PERSONAL: " << iDpersonal << endl;
 }
 
 // SETS
@@ -52,6 +57,7 @@ void Turno::setIdTurno(int t){idTurno = t;}
 void Turno::setFechaServicio(Fecha f){fechaServicio = f;}
 void Turno::setIdCuenta(int d){idCuenta = d;}
 void Turno::setTipoServicio(int s){tipoServicio = s;}
+void Turno::setIdPersonal(int p){iDpersonal = p;}
 void Turno::setEstado(bool e){estado = e;}
 
 // GETS
@@ -59,4 +65,5 @@ int Turno::getIdTurno(){return idTurno;}
 Fecha Turno::getFechaServicio(){return fechaServicio;}
 int Turno::getIdCuenta(){return idCuenta;}
 int Turno::getTipoServicio(){return tipoServicio;}
+int Turno::getIdPersonal(){return iDpersonal;}
 bool Turno::getEstado(){return estado;}
